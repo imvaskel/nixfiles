f2b1d4fa7de3f971c7cb4442f9a70753438ed77d
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-11-05 16:55:19 -0500

    update inputs, system.defaults

---
+++ modules/darwin/configs.nix
@@ -0,0 +1,32 @@
+{...}: {
+  system.defaults = {
+    dock = {
+      tilesize = 52;
+      autohide = true;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.25;
+    };
+    finder = {
+      _FXSortFoldersFirst = true;
+      FXEnableExtensionChangeWarning = false;
+      NewWindowTarget = "Home";
+      AppleShowAllExtensions = true;
+      QuitMenuItem = true;
+    };
+    NSGlobalDomain = {
+      NSDocumentSaveNewDocumentsToCloud = false;
+      NSTableViewDefaultSizeMode = 2;
+    };
+    loginwindow.GuestEnabled = false;
+    CustomUserPreferences = {
+      "com.apple.finder" = {
+        ShowHardDrivesOnDesktop = false;
+        ShowExternalHardDrivesOnDesktop = false;
+        ShowMountedServersOnDesktop = false;
+        ShowRemovableMediaOnDesktop = false;
+        ShowRecentTags = false;
+        FXRemoveOldTrashItems = true;
+      };
+    };
+  };
+}
