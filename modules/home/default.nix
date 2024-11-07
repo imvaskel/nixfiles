c3de54f77fbf0c2964747dab828e5064d2235d46
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-11-07 11:22:03 -0500

    vscode

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,14 +1,26 @@
 {
   lib,
   flake,
+  config,
+  pkgs,
   ...
 }: let
   inherit (flake.inputs) self;
+  cfg = config.dotfiles.type;
+
+  packages = [];
+  graphicalPackages = with pkgs; [
+   vscode 
+    ];
+  
+    allPackages = packages ++ (lib.optionals cfg.graphical graphicalPackages);
 
   # kind of cursed to be honest but i love it
   imports = lib.filterAttrs (k: v: k != "default") self.homeModules;
 in {
   imports = lib.attrValues imports;
+
+    home.packages = allPackages;
 
   nixpkgs.config.allowUnfree = true;
 
